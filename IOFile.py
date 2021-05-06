047492cd69023cf48074afb408a07d48f736c18e
jhello <jhello@126.com>
2021-05-07 00:37:06 +0800

    add compare figure before and after svd

--- IOFile.py
+++ IOFile.py
@@ -5,13 +5,47 @@ Created on Tue Dec 15 20:47:14 2020
 
 @author: jhello
 """
-
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
+from sortedcontainers import SortedSet
 import configparser
+import linecache
+import pandas as pd
+
+
+def plot_solution(dfs, time_count, space_count, var_name):
+    """
+    Plot 3D for specified variable.
+    :param dfs:
+    :param time_count:
+    :param space_count:
+    :param var_name:
+    :return:
+    """
+    x = dfs['x'].values.reshape([time_count, space_count])
+    y = dfs['t'].values.reshape([time_count, space_count])
+    z = dfs[var_name].values.reshape([time_count, space_count])
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    # Note: Constant value cause warning when plot contourf.
+    ax.contourf(x, y, z, zdir='z', offset=-1, cmap=cm.coolwarm)
+    ax.contourf(x, y, z, zdir='y', offset=0.5, cmap=cm.coolwarm)
+    surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, rstride=1, cstride=1, alpha=0.8)
+    ax.set_xlabel('x')
+    ax.set_ylabel('t')
+    ax.set_zlabel(var_name)
+    ax.set_ylim(0, 0.5)
+    ax.set_zlim(-1, 1)
+    fig.colorbar(surf, shrink=0.5, aspect=5, ax=ax)
+
+    plt.show()
+
 
 class IniFile:
     def __init__(self):
         self._config = configparser.ConfigParser(delimiters=' ')
-        self._config.optionxform=str
+        self._config.optionxform = str
 
         self._config['Grid'] = {}
         self._config['Chombo Refinement'] = {}
@@ -100,13 +134,108 @@ class IniFile:
 
     def set_time_mesh_count(self, count):
         tstop = float(self._config['Time']['tstop'])
-        tab = tstop/count
+        tab = tstop / count
         self._config['Static Grid Output']['tab'] = '  '.join([str(tab), '-1'])
 
     def set_output_dir(self, output_dir):
         self._config['Static Grid Output']['output_dir'] = output_dir
 
 
-class TabFile:
-    def __init__(self):
+class DataTabFile:
+    def __init__(self, name):
+        self._name = name
+
+    def dataframe(self):
+        # dataframe headers
+        df_names = 'x t rho vx1 vx2 vx3 Bx1 Bx2 Bx3 prs'.split()
+        return pd.read_table(self._name, delim_whitespace=True, names=df_names)
+
+
+class DataTabFiles:
+    def __init__(self, result_tmp_dir, time_steps):
+        self._result_tmp_dir = result_tmp_dir
+        self._time_steps = time_steps
+        self._solution = pd.DataFrame()
+
+    def solution(self):
+        df_list = []
+        for time_index, time_step in enumerate(self._time_steps):
+            tab_file_name = self._result_tmp_dir + 'data.{:04d}.tab'.format(time_index)
+            df = DataTabFile(tab_file_name).dataframe()
+            # assign time step to 'y' column
+            df['t'] = time_step
+            df_list += [df]
+
+        # concatenate all dataframes to a long big one
+        self._solution = pd.concat(df_list, ignore_index=True)
+        return self._solution
+
+    def a_style_dataframe(self):
+        """
+        Returns solution dataframe as
+                                t_1 t_2 ... t_n
+        node_1 para_1
+               para_2
+               para_3
+               ...
+               para_x
+
+        node_2 para_1
+               para_2
+               para_3
+               ...
+               para_x
+
+        ...
+
+        node_m para_1
+               para_2
+               para_3
+               ...
+               para_x
+
+        :return:
+        """
+        para_names = 'rho vx1 vx2 vx3 Bx1 Bx2 Bx3 prs'.split()
+        if self._solution.empty:
+            self._solution = self.solution()
+        time_steps = list(SortedSet(self._solution['t']))
+        space_steps = list(SortedSet(self._solution['x']))
+
+        a_style_dfs = []
+        for space_step in space_steps:
+            src_df = self._solution.loc[self._solution['x'] == space_step]
+            new_index_df = src_df.set_index('t')
+            transposed_df = new_index_df.transpose()
+            tgt = transposed_df.loc[transposed_df.index.isin(para_names)]
+            tgt_without_all_0_rows = tgt.loc[(tgt.T != 0).any()]
+            a_style_dfs += [tgt_without_all_0_rows]
+
+        return pd.concat(a_style_dfs)
+
+    def b_style_dataframe(self):
         pass
+
+
+class TabOutFile:
+    """The tab.out file.
+    """
+
+    def __init__(self, name):
+        self._name = name
+
+    def time_steps(self):
+        # read time step from 'tab.out' file
+        return pd.read_table(self._name, delim_whitespace=True, usecols=[1], header=None)[1]
+
+
+class GridOutFile:
+    """The grid.out file.
+    """
+
+    def __init__(self, name):
+        self._name = name
+
+    def space_count(self):
+        # read space step count from 'grid.out' file (No. 9 line)
+        return int(linecache.getline(self._name, 9))
--- main.py
+++ main.py
@@ -6,8 +6,12 @@ Created on Tue Dec 15 12:05:48 2020
 @author: jhello
 """
 
-from IOFile import IniFile, TabFile
-import os, shutil, subprocess
+import os
+import shutil
+import subprocess
+
+from IOFile import IniFile
+
 
 def clear_output_dir(result_dir, temp_dir):
     if os.path.isdir(result_dir):
@@ -17,6 +21,7 @@ def clear_output_dir(result_dir, temp_dir):
     os.mkdir(temp_dir)
     os.chdir('..')
 
+
 def create_ini_file(space_mesh_count, output_dir, ini_file_name, time_mesh_count):
     ini_file = IniFile()
     ini_file.set_space_mesh_count(space_mesh_count)
@@ -24,10 +29,12 @@ def create_ini_file(space_mesh_count, output_dir, ini_file_name, time_mesh_count
     ini_file.set_output_dir(output_dir)
     ini_file.write(ini_file_name)
 
+
 def store_output():
     # Read the output files,
     # compress them and store in one file.
     pass
+
 
 def main():
     # path of pluto executive file
@@ -45,18 +52,19 @@ def main():
     # physical variables result given at the middle of each element
     space_mesh_count = 800
     # time mesh count, not time step, a result file is output at the mesh point
-    time_mesh_count = 30
+    time_mesh_count = 40
     # reset problem by remove the output folder
     clear_output_dir(result_dir, temp_dir)
     # create pluto input file of pluto with certain params
-    create_ini_file(ini_file_name = ini_file_name,
-                    output_dir = output_dir,
-                    space_mesh_count = space_mesh_count,
-                    time_mesh_count = time_mesh_count)
+    create_ini_file(ini_file_name=ini_file_name,
+                    output_dir=output_dir,
+                    space_mesh_count=space_mesh_count,
+                    time_mesh_count=time_mesh_count)
     # call pluto to compute
-    #output = \
+    # output = \
     subprocess.check_output([pluto_exec])
-    #print(output.decode())
+    # print(output.decode())
+
 
 if __name__ == '__main__':
     # story starts here
